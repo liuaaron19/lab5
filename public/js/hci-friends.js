d348a48fe8fae59546d7e7486651a4ea4ce7c3bd
Aaron Liu <a3liu@ucsd.edu>
2018-02-08 14:20:24 -0800

    finished?

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+    $("a.anaName").click(projectClick);
 }
 
 function anagrammedName(name) {
@@ -43,4 +44,11 @@ function anagrammedName(name) {
 		console.log(name + " not known for anagramming.");
 		return name;
 	}
+}
+
+function projectClick(e) {
+    e.preventDefault();
+    var name = $(this).text();
+    var newName = anagrammedName(name);
+    $(this).text(newName);
 }
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,18 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+exports.addFriend = function(req, res) {
+    var newName = req.query.name;
+    var newDes = req.query.description;
+    var newImg = "http://lorempixel.com/400/400/people"; 
+
+    console.log(newName + '|' + newDes + '|' + newImg);
+    
+    var newFriend = { 
+        'name' : newName,
+        'description' : newDes,
+        'imageURL' : newImg,
+    };
+
+    data.friends.push(newFriend);
+}
+
